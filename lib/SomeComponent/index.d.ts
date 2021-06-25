cb97d093ea0e6ed48440b7049ac23201952eb170
Claudio Romero <25067140+romerorocha@users.noreply.github.com>
2021-06-24 21:42:10 -0300

    Remove 'lib' dir

--- lib/SomeComponent/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export declare type SomeComponentProps = {
-    title?: string;
-    body?: string;
-};
-export declare const SomeComponent: (props: SomeComponentProps) => JSX.Element;
