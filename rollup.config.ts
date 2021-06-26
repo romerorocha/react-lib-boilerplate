c49dd1536e748740ba114ebae064159d5ac940f5
Claudio Romero <25067140+romerorocha@users.noreply.github.com>
2021-06-26 10:10:32 -0300

    Babel config cleanup

--- rollup.config.ts
+++ rollup.config.ts
@@ -8,30 +8,21 @@ import { terser } from 'rollup-plugin-terser'
 import packageJson from './package.json'
 
 const plugins = [peerDepsExternal(), resolve(), commonjs(), postcss(), terser()]
+const exclude = ['node_modules', 'lib', 'src/**/*stories.tsx', 'src/**/*.test.*']
+const tsConfig = { declaration: true, declarationDir: './lib', rootDir: 'src/', exclude }
 
 export default [
   // CommonJS
   {
     input: 'src/index.ts',
     output: { dir: './', entryFileNames: packageJson.main, format: 'cjs' },
-    plugins: [
-      ...plugins,
-      typescript({
-        declaration: true,
-        declarationDir: './lib',
-        rootDir: 'src/',
-        exclude: ['node_modules', 'lib', 'src/**/*stories.tsx', 'src/**/*.test.*'],
-      }),
-    ],
+    plugins: [...plugins, typescript(tsConfig)],
   },
 
   // ES
   {
     input: 'src/index.ts',
     output: { file: packageJson.module, format: 'esm' },
-    plugins: [
-      ...plugins,
-      typescript({ exclude: ['node_modules', 'lib', 'src/**/*stories.tsx', 'src/**/*.test.*'] }),
-    ],
+    plugins: [...plugins, typescript({ exclude })],
   },
 ]
