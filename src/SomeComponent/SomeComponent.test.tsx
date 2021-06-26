1e13a8190c3e60aa08bf069854bd8068f1e3f4e5
Claudio Romero <25067140+romerorocha@users.noreply.github.com>
2021-06-26 10:57:57 -0300

    Add snapshot test

--- src/SomeComponent/SomeComponent.test.tsx
+++ src/SomeComponent/SomeComponent.test.tsx
@@ -1,10 +1,13 @@
-import { getByRole, render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { SomeComponent } from '.'
 
 describe('<SomeComponent />', () => {
   it('should render default title and body', () => {
-    const { getByRole, getByText } = render(<SomeComponent />)
+    const { getByRole, getByText, container } = render(<SomeComponent />)
+
     getByRole('heading', { name: /hello!/i })
     getByText(/hello, world!/i)
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
